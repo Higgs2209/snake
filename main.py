c09966f9071c5d931c24091b6e286f38abfce23f
connor <connord79@gmail.com>
2024-01-04 20:34:43 +1100

    Snake Movement, Apple spawning and score

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 import pygame
 from pygame.locals import *
 import time
+import random
+
 
 SIZE = 40
+
+
+class Apple:
+    def __init__(self, parent_screen):
+        self.image = pygame.image.load("resources/apple.jpg").convert()
+        self.parent_screen = parent_screen
+        self.x = SIZE*3
+        self.y = SIZE*3
+
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    def move(self):
+        self.x = random.randint(0, 24) * SIZE
+        self.y = random.randint(0, 19) * SIZE
+
 
 class Snake:
     def __init__(self, parent_screen, length):
@@ -12,6 +31,11 @@ class Snake:
         self.x = [SIZE] * length
         self.y = [SIZE] * length
         self.direction = 'down'
+
+    def increase_length(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
 
     def move_left(self):
         self.direction = 'left'
@@ -57,6 +81,28 @@ class Game:
         self.surface.fill((110, 110, 5))
         self.snake = Snake(self.surface, 2)
         self.snake.draw()
+        self.apple = Apple(self.surface)
+        self.apple.draw()
+
+    def is_collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+
+
+    def play(self):
+        self.snake.walk()
+        self.apple.draw()
+        self.display_score()
+        pygame.display.flip()
+        if self.is_collision(self.snake.x[0], self.snake.y[0], self.apple.x, self.apple.y):
+            self.snake.increase_length()
+            self.apple.move()
+
+    def display_score(self):
+        font = pygame.font.SysFont("arial", 30)
+        score = font.render(f"Score: {self.snake.length}", True, (200, 200, 200))
+        self.surface.blit(score, (800, 10))
 
     def run(self):
         running = True
@@ -79,7 +125,10 @@ class Game:
                 elif event.type == QUIT:
                     running = False
 
-            self.snake.walk()
+            self.play()
+
+
+
             time.sleep(0.4)
 
 
